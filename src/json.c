13d06e1313622af5601f99cc6709091dac127101
Georges Kadlubski <georgeskadlubski@gmail.com>
2025-10-05 15:02:16 +0200

    Add: first structure

---
+++ src/json.c
@@ -0,0 +1,8 @@
+//###########################################################################
+//# json_c_library: A c library to parse, modify and generate json strings. #
+//# https://github.com/kadlubski88/json_c_library                           #
+//#                                                                         #
+//# The MIT License (MIT)                                                   #
+//# Copyright © 2025 Georges Kadlubski                                      #
+//# URL: https://mit-license.org/                                           #
+//###########################################################################
