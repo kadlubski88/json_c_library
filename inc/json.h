1214f0161804576b4810f0977d660c6df0c797c8
Georges Kadlubski <georgeskadlubski@gmail.com>
2025-10-06 15:27:32 +0200

    Add: tokeniser function

--- inc/json.h
+++ inc/json.h
@@ -1,8 +1,55 @@
+//###########################################################################
+//# json_c_library: A c library to parse, modify and generate json strings. #
+//# https://github.com/kadlubski88/json_c_library                           #
+//#                                                                         #
+//# The MIT License (MIT)                                                   #
+//# Copyright © 2025 Georges Kadlubski                                      #
+//# URL: https://mit-license.org/                                           #
+//###########################################################################
+
 #ifndef _HEADER_JSON_H_
 #define _HEADER_JSON_H_
 
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
-void hello(void);
+#define PERROR fprintf(stderr, "Error in file %s at line %d: %s\n", __FILE__, __LINE__, strerror(errno))
 
-#endif
+#define PARSING_SUCCESS 0
+#define PARSING_FAILURE 1
+
+//###################
+//# Enum definition #
+//###################
+
+typedef enum{
+    TOKEN_BLOCK_START,
+    TOKEN_BLOCK_END,
+    TOKEN_ARRAY_START,
+    TOKEN_ARRAY_END,
+    TOKEN_STRING,
+    TOKEN_NUMBER,
+    TOKEN_BOOL_TRUE,
+    TOKEN_BOOL_FALSE,
+    TOKEN_COLON,
+    TOKEN_COMMA,
+    TOKEN_NULL,
+    TOKEN_END,
+    TOKEN_UNKNOWN
+}token_type;
+
+typedef enum{
+    STATE_KEY,
+    STATE_VALUE,
+}key_value_state;
+
+//######################
+//# function prototype #
+//######################
+
+token_type get_token(char **input, char **token_value, int *token_length);
+
+#endif //_HEADER_JSON_H_
--- src/json.c
+++ src/json.c
@@ -9,7 +9,86 @@
 
 #include "../inc/json.h"
 
-void hello(void) {
-    printf("Hello Welt!!!");
-    return;
+token_type get_token(char **input, char **token_value, int *token_length) {
+    // reset value length;
+    *token_length = 0;
+    // skip white spaces and new line
+    while (**input && (**input == 0x20 || **input == '\n')) {
+        (*input)++;
+    }
+    // tokenise
+    switch (**input) {
+    case '{':
+        (*input)++;
+        return TOKEN_BLOCK_START;
+    case '}':
+        (*input)++;
+        return TOKEN_BLOCK_END;
+    case '[':
+        (*input)++;
+        return TOKEN_ARRAY_START;
+    case ']':
+        (*input)++;
+        return TOKEN_ARRAY_END;
+    case ':':
+        (*input)++;
+        return TOKEN_COLON;
+    case ',':
+        (*input)++;
+        return TOKEN_COMMA;
+    case '"':
+        (*input)++;
+        *token_value = *input;
+        while (**input && **input != '"') {
+            (*token_length)++;
+            (*input)++;
+        }
+        if (**input == '"') {
+            (*input)++;
+        }
+        // Maximal string length?
+        return TOKEN_STRING;
+    case 't':
+        if (strncmp(*input, "true", 4) == 0) {
+            (*input) += 4;
+            return TOKEN_BOOL_TRUE;
+        }
+    case 'f':
+        if (strncmp(*input, "false", 5) == 0) {
+            (*input) += 5;
+            return TOKEN_BOOL_FALSE;
+        }
+    case 'n':
+        if (strncmp(*input, "null", 4) == 0) {
+            (*input) += 4;
+            return TOKEN_NULL;
+        } else {
+            // not bool
+            (*input)++;
+            return TOKEN_UNKNOWN;
+        }
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+        *token_value = *input;
+        (*token_length)++;
+        (*input)++;
+        while ((**input <= '9' && **input >= '0') || **input == '.') {
+            (*token_length)++;
+            (*input)++;
+        }
+        return TOKEN_NUMBER;
+    case '\0':
+        return TOKEN_END;
+    default:
+        (*input)++;
+        return TOKEN_UNKNOWN;
+    }
 }
